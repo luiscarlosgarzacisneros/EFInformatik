dc5b7f3510d9048e5047b209b5677ac3b3bc8218
luis <luiscarlos.garzacisneros@edu.gfbienne.ch>
2022-09-09 13:27:39 +0200

    primzahlen und dateiendungen

--- docs/exercises/primzahlen.py
+++ docs/exercises/primzahlen.py
@@ -5,5 +5,16 @@ primzahlen = []
 ka = True
 
 
-for i in range(100):
-    zahlen.append
+for i in range(2, 100):
+    zahlen.append(i)
+
+for q in zahlen:
+    for w in primzahlen:
+        if q % w == 0:
+            ka = True
+
+    if ka == True:
+        primzahlen.append(q)
+    ka == True
+
+print(primzahlen)
