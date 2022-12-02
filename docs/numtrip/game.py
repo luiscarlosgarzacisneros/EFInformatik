7411990ebee9a6e255be26ccb8b70d692fa8bc08
luis <luiscarlos.garzacisneros@edu.gfbienne.ch>
2022-12-02 13:46:13 +0100

    numtrip game.py anzahl leerzeichen effizienter und rückmeldung eingabe falsch

--- docs/numtrip/game.py
+++ docs/numtrip/game.py
@@ -1,7 +1,7 @@
 
 board = [
-        [2, 4, 666, 88, 8],
-        [4, 222222, 8, 2, 1],
+        [88, 88, 666, 88, 88],
+        [88, 222222, 8, 2, 1],
         [4, 69, 88, 4, 2],
         [2, 88, 1111, 44444, 1],
         [2, 4, 444, 444, 4]
@@ -23,35 +23,11 @@ def board1():
         artzeile = artzeile + 1
 
         for o in range(5):
-            if len(str(board[zeile][spalte])) == 1:
-                print('I   ', end='')
-                print(board[zeile][spalte], end='')
-                print('  ', end='')
-
-            if len(str(board[zeile][spalte])) == 2:
-                print('I  ', end='')
-                print(board[zeile][spalte], end='')
-                print('  ', end='')
-
-            if len(str(board[zeile][spalte])) == 3:
-                print('I  ', end='')
-                print(board[zeile][spalte], end='')
+            anzleer = 6 - int(len(str(board[zeile][spalte])))
+            print('I', end='')
+            for g in range(anzleer):
                 print(' ', end='')
-
-            if len(str(board[zeile][spalte])) == 4:
-                print('I ', end='')
-                print(board[zeile][spalte], end='')
-                print(' ', end='')
-
-            if len(str(board[zeile][spalte])) == 5:
-                print('I ', end='')
-                print(board[zeile][spalte], end='')
-                print('', end='')
-
-            if len(str(board[zeile][spalte])) == 6:
-                print('I', end='')
-                print(board[zeile][spalte], end='')
-                print('', end='')
+            print(board[zeile][spalte], end='')
 
             spalte = spalte + 1
 
@@ -76,29 +52,36 @@ def feldlöschen():
 
     if not zeile.isnumeric():
         eingabe_zeile = False
+        fehler = 'eingabe muss eine zahl sein'
     if not spalte.isnumeric():
         eingabe_spalte = False
+        fehler = 'eingabe muss eine (ganze) zahl sein'
 
     if zeile.isnumeric():
         zeile = int(zeile)
 
-        if zeile < 0:
+        if zeile < 1:
             eingabe_zeile = False
+            fehler = 'zahl muss grösser als 1 sein'
         if zeile > 5:
             eingabe_zeile = False
+            fehler = 'zahl muss kleiner als 5 sein'
 
     if spalte.isnumeric():
         spalte = int(spalte)
 
-        if spalte < 0:
+        if spalte < 1:
             eingabe_spalte = False
+            fehler = 'zahl muss grösser als 1 sein'
         if spalte > 5:
             eingabe_spalte = False
+            fehler = 'zahl muss kleiner als 5 sein'
 
     if eingabe_zeile and eingabe_spalte == True:
         board[zeile - 1][spalte - 1] = ' '
     else:
         print('Fehlerhafte Eingabe')
+        print(fehler)
 
 
 spielen = True
