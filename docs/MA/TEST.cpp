02c8e55731f5afcf06b1b703b9f193c15828dc5e
luiscarlosgarzacisneros <LuisCarlos.GarzaCisneros@edu.gbjb.ch>
2023-09-15 18:17:44 +0200

    s

--- docs/MA/TEST.cpp
+++ docs/MA/TEST.cpp
@@ -11,6 +11,80 @@
 #include <string>
 
 
+void print_bitboard(const uint64_t bitboard) {
+    for (int rank=7; rank>=0; --rank) {
+        for (int file=0; file<8; ++file) {
+            int square = rank*8+file;
+            uint64_t m = 1ULL<<square;
+            if (bitboard & m) {std::cout<<"1 ";}
+            else {std::cout<<"0 ";}
+        }
+        std::cout << std::endl;
+    }
+}
+
+//
+
+bool is_one_at_this_index(const uint64_t bitboard, int index) {
+    uint64_t m = 1ULL << index; //m=00000....001 1 wird um index rightshifted
+    return (((bitboard & m) != 0));
+    
+}
+
+int yx_zu_index(int y, int x) {
+    return (y*8+x); //fangt unten rechts an!
+}
+
+bool is_in_board(int y, int x) {
+    return ((x>-1 && x<8) && (y>-1 && y<8));
+}
+
+uint64_t shift_bit(uint64_t bitboard, int vy, int vx, int zy, int zx) {//wenn (vy, vx)==1
+    //clear bit at (vy, vx)
+    bitboard &= ~(1ULL << (vy * 8 + vx));
+    //set bit at (zy, zx) to 1
+    bitboard |= (1ULL << (zy * 8 + zx));
+    return bitboard;
+}
+
+std::vector<uint64_t> gcLl(int player, const uint64_t knight_bitboard, const uint64_t this_players_pieces) {
+    std::vector<uint64_t> childrenLl;
+    print_bitboard(knight_bitboard);
+    std::cout<<"--"<<std::endl;
+    //
+    int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
+    int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
+    
+    //get knights current position as (x, y) coord.
+    std::vector<std::pair<int,int>> knight_positions;
+    for (int y=0; y<8; ++y) {
+        for (int x=0; x<8; ++x) {
+            if (is_one_at_this_index(knight_bitboard, yx_zu_index(y, x))) {knight_positions.push_back({y, x});}
+        }
+    }
+    //modify boards
+    for (const auto& position : knight_positions) {
+        int y_current = position.first;
+        int x_current = position.second;
+        //
+        for (int i=0; i<8; ++i) {
+            int y_new = y_current + dy[i];
+            int x_new = x_current + dx[i];
+            //
+            if (is_in_board(y_new, x_new)) {
+                if (!(is_one_at_this_index(this_players_pieces, yx_zu_index(y_new, x_new)))) {
+                    uint64_t child=shift_bit(knight_bitboard, y_current, x_current, y_new, x_new);
+                    childrenLl.push_back(child);
+                }
+            }
+        }
+    }
+    //
+    return childrenLl;
+}
+
+
+
 class Board {
 public:
 
@@ -39,90 +113,6 @@ public:
 
 
     Board() {}
-
-    void print_bitboard(uint64_t bitboard) {
-        for (int rank=7; rank>=0; --rank) {
-            for (int file=0; file<8; ++file) {
-                int square = rank*8+file;
-                uint64_t m = 1ULL<<square;
-                if (bitboard & m) {std::cout<<"1 ";}
-                else {std::cout<<"0 ";}
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    //
-
-    bool is_one_at_this_index(uint64_t bitboard, int index) {
-        uint64_t m = 1ULL << index; //m=00000....001 1 wird um index rightshifted
-        return (bitboard & m) != 0;
-    }
-
-    int yx_zu_index(int y, int x) {
-        return (y*8+x); //fangt unten rechts an!
-    }
-
-    bool is_in_board(int y, int x) {
-        return ((x>-1 && x<8) && (y>-1 && y<8));
-    }
-
-    uint64_t shift_bit(uint64_t bitboard, int vy, int vx, int zy, int zx) {//wenn (vy, vx)==1
-        //clear bit at (vy, vx)
-        bitboard &= ~(1ULL << (vy * 8 + vx));
-        //set bit at (zy, zx) to 1
-        bitboard |= (1ULL << (zy * 8 + zx));
-        return bitboard;
-    }
-
-    std::vector<uint64_t> gcLl(int player, uint64_t knight_bitboard) {
-        std::vector<uint64_t> childrenLl;
-        //
-        int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
-        int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
-        
-        //get knights current position as (x, y) coord.
-        int y;
-        int x;
-        std::vector<std::pair<int,int>> knight_positions;
-        uint64_t m=1ULL;
-        for (y=0; y<8; ++y) {
-            for (x=0; x<8; ++x) {
-                if (is_one_at_this_index(knight_bitboard, yx_zu_index(y, x))) {knight_positions.push_back({y, x});}
-            }
-        }
-        //
-        uint64_t this_players_pieces;
-        uint64_t other_players_pieces;
-        if (player==2) {
-            this_players_pieces=this->white_pieces;
-            other_players_pieces=this->black_pieces;
-        }
-        else {
-            this_players_pieces=this->black_pieces;
-            other_players_pieces=this->white_pieces;
-        }
-        //
-        //modify boards
-        for (const auto& position : knight_positions) {
-            int y_current = position.first;
-            int x_current = position.second;
-            //
-            for (int i=0; i<8; ++i) {
-                int y_new = y_current + dy[i];
-                int x_new = x_current + dx[i];
-                //
-                if (is_in_board(y_new, x_new)) {
-                    if (!(is_one_at_this_index(this_players_pieces, yx_zu_index(y_new, x_new)))) {
-                        uint64_t child=shift_bit(knight_bitboard, y_current, x_current, y_new, x_new);
-                        childrenLl.push_back(child);
-                    }
-                }
-            }
-        }
-        //
-        return childrenLl;
-    }
 
 };
 
@@ -155,10 +145,14 @@ main() {
     root_node.white_pieces  = 0b0000000000000000000000000000000000000000000000001111111111111111ULL;
     root_node.black_pieces  = 0b1111111111111111000000000000000000000000000000000000000000000000ULL;
 
-    for (const auto& child : root_node.gcLl(root_node.w_knight, 2)) {
-        root_node.print_bitboard(child);
-        std::cout<<"----"<<std::endl;
+    print_bitboard(root_node.w_knight);
+    for (const auto& child : gcLl(2, root_node.w_knight, root_node.white_pieces)) {
+        //root_node.print_bitboard(child);
+        //std::cout<<"----"<<std::endl;
+        ;
     }
-    root_node.print_bitboard(root_node.w_knight);
+    print_bitboard(root_node.w_knight);
+    
 
 }
+
